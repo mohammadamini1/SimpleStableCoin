1a03ed4f7cff6a8b18882cf0fefe8d524f1e31bf
Mohammad Amini <amini.muhammadh@gmail.com>
2025-01-04 09:02:40 -0500

    project init

---
+++ src/SimGov.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+// Import OpenZeppelin ERC20 implementation and AccessControl
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+
+contract SimGov is ERC20, AccessControl {
+
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+    }
+
+
+}
+
+
+
---
+++ src/Vault.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Import OpenZeppelin libraries
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Vault is AccessControl {
+
+}
+
+
